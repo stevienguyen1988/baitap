a1046cb980b130f3a7d280f48700b74ed94fb19e
stevienguyen <nguyenhongthai1988@gmail.com>
2013-05-02 20:24:13 +0700

    bai moi

---
+++ PureTableView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PureTableView
+//
+//  Created by techmaster on 5/2/13.
+//  Copyright (c) 2013 TechMaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
